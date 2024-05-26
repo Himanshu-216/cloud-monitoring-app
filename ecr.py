21817eb7360127fb297571ff4491a09a424ffe64
Himanshu <himanshu162pnt@gmail.com>
2024-05-26 10:34:51 +0530

    wip

--- ecr.py
+++ ecr.py
@@ -2,8 +2,7 @@ import boto3
 
 ecr_client = boto3.client('ecr')
 
-repository_name = "my_monitoring_app_image"
-response = ecr_client.create_repository(repositoryName=repository_name)
+response = ecr_client.create_repository(repositoryName="ecr_repo_flask")
 
 repository_uri = response ['repository']['repositoryUri']
 print(repository_uri)
